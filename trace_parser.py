b4ced2b9315e127e1b60492674f06da3e5ada247
Totorody <arbc139@gmail.com>
2018-04-25 21:26:39 +0900

    Adds replace test.

--- trace_parser.py
+++ trace_parser.py
@@ -3,7 +3,7 @@ def parse(trace_file):
   result = []
   while True:
     line = trace_file.readline()
+    line = line.replace('\n', "")
     if not line: break
     result.append(line)
   return result
-
