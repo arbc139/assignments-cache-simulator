317a04b2a7cbdc7f4a4f4730e497fc328bc3cfd4
Totorody <arbc139@gmail.com>
2018-04-25 23:13:10 +0900

    Progress...

--- trace_parser.py
+++ trace_parser.py
@@ -1,14 +1,13 @@
 
-from gmpy2 import mpz
-
 def parse(trace_file):
   result = []
   while True:
     line = trace_file.readline()
     tokenized_line = line.replace('\n', '').split()
     if not line: break
+
     result.append({
       'type': int(tokenized_line[0]),
-      'address': mpz(int(tokenized_line[1], 16)).setbit(64),
+      'address': int(tokenized_line[1], 16),
     })
   return result
