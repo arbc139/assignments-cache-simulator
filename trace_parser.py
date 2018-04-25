1866462a27b2091bcdc6ac3b80237d4359d947ec
Totorody <arbc139@gmail.com>
2018-04-25 21:36:21 +0900

    Implements tokenizer in trace parser.

--- trace_parser.py
+++ trace_parser.py
@@ -3,7 +3,10 @@ def parse(trace_file):
   result = []
   while True:
     line = trace_file.readline()
-    line = line.replace('\n', "")
+    tokenized_line = line.replace('\n', '').split()
     if not line: break
-    result.append(line)
+    result.append({
+      'mode': tokenized_line[0],
+      'address': tokenized_line[1],
+    })
   return result
